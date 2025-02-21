05a5242f3fd37cf2ae3813a3e87993553e16faa2
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-21 23:35:09 +0900

    add: the first part of cdk pipelines tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'ExamplePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('yutaro-sakamoto/aws-cicd-example', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    })
 
     // define resources here...
   }
